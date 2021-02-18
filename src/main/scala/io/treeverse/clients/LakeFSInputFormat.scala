12ba36b6bd4d68da3617658362c47b56e86552d3
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-02-18 18:54:57 +0200

    Read and deserialize actual protobufs

--- src/main/scala/io/treeverse/clients/LakeFSInputFormat.scala
+++ src/main/scala/io/treeverse/clients/LakeFSInputFormat.scala
@@ -1,68 +1,95 @@
-  package io.treeverse.clients
+package io.treeverse.clients
 
 import io.treeverse.committed.Committed.RangeData
+import io.treeverse.catalog.Catalog
+
 import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.Text
-import org.apache.hadoop.mapred._ // TODO (johnnyaug)  use only "madereduce", not mapred
+import org.apache.hadoop.mapreduce.{InputSplit, RecordReader, TaskAttemptContext}
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.InputFormat
+import org.apache.hadoop.mapred.SplitLocationInfo
+import org.apache.hadoop.mapreduce.JobContext
+import org.apache.hadoop.mapreduce.lib.input.FileSplit
 
 import java.io.File
 
-class EntryRecordReader(ssTableReader: SSTableReader, localFile: String) extends RecordReader[Text, Text] {
-  private val it = ssTableReader.getData(localFile)
+import scala.collection.JavaConverters._
+import com.google.protobuf.Message
 
-  override def next(key: Text, value: Text): Boolean = {
+class GravelerSplit(val range: RangeData) extends InputSplit {
+  override def getLength: Long = range.getEstimatedSize
+
+  override def getLocations: Array[String] = null
+
+  override def getLocationInfo(): Array[SplitLocationInfo] = null
+}
+
+class WithIdentifier[Proto <: Message](val id: Array[Byte], val message: Proto) {}
+
+class EntryRecordReader[Proto <: Message](ssTableReader: SSTableReader[Proto]) extends RecordReader[Array[Byte], WithIdentifier[Proto]] {
+  var it: SSTableIterator[Proto] = null
+  var item: Item[Proto] = null
+
+  override def initialize(split: InputSplit, context: TaskAttemptContext) = {
+    val range = split.asInstanceOf[GravelerSplit]
+    it = ssTableReader.newIterator()
+  }
+
+  override def nextKeyValue: Boolean = {
     if (!it.hasNext) {
       return false
     }
-    key.set(it.next().key)
-    true
+    item = it.next
+    return true
   }
 
-  override def createKey(): Text = {
-    new Text()
-  }
+  override def getCurrentKey = item.key
 
-  override def createValue(): Text = {
-    new Text()
-  }
+  override def getCurrentValue() = new WithIdentifier(item.id, item.message)
 
-  override def getPos: Long = {
-    0 // TODO(johnnyaug) complete
-  }
-
-  override def close(): Unit = {
-    new File(localFile).delete
-  }
+  override def close = it.close
 
   override def getProgress: Float = {
     0 // TODO(johnnyaug) complete
   }
 }
 
-object LakeFSInputFormat {}
+object LakeFSInputFormat {
+  private def read[Proto <: Message](reader: SSTableReader[Proto]): Seq[Item[Proto]] =
+    reader.newIterator.toSeq
+}
 
-class LakeFSInputFormat extends InputFormat[Text, Text] {
-  override def getSplits(job: JobConf, numSplits: Int): Array[InputSplit] = {
-    val metaRangePath = job.get(FileInputFormat.INPUT_DIR)
+class LakeFSInputFormat extends InputFormat[Array[Byte], WithIdentifier[Catalog.Entry]] {
+  import LakeFSInputFormat._
+
+  override def getSplits(job: JobContext): java.util.List[InputSplit] = {
+    val metaRangePath = job.getConfiguration.get(FileInputFormat.INPUT_DIR)
     val p = new Path(metaRangePath)
-    val fs = p.getFileSystem(job)
+    val fs = p.getFileSystem(job.getConfiguration)
     val localFile = File.createTempFile("lakefs", "metarange")
     fs.copyToLocalFile(p, new Path(localFile.getAbsolutePath))
-    val ranges = new SSTableReader().get(localFile.getAbsolutePath, RangeData.newBuilder().build())
-    localFile.delete()
     // TODO(johnnyaug) ask lakeFS metadata server for the sstable url
-    ranges.map(r => new FileSplit(new Path(p.getParent + "/" + new String(r.identity)), 0, 0, null.asInstanceOf[Array[String]])).toArray
+    val rangesReader = new SSTableReader(localFile.getAbsolutePath, RangeData.newBuilder().build())
+    localFile.delete()
+
+    val ranges = read(rangesReader)
+
+    ranges.map(
+      r => new FileSplit(new Path(p.getParent + "/" + new String(r.id)), 0, 0, null.asInstanceOf[Array[String]])
+        // Scala / JRE not strong enough to handle List<FileSplit> as List<InputSplit>;
+        // explicitly upcast to generate Seq[InputSplit].
+        .asInstanceOf[InputSplit]
+    ).asJava
   }
 
-  override def getRecordReader(split: InputSplit, job: JobConf, reporter: Reporter): RecordReader[Text, Text] = {
+  override def createRecordReader(split: InputSplit, context: TaskAttemptContext): RecordReader[Array[Byte], WithIdentifier[Catalog.Entry]] = {
     val localFile = File.createTempFile("lakefs", "range")
     localFile.deleteOnExit()
     val fileSplit = split.asInstanceOf[FileSplit]
-    val fs = fileSplit.getPath.getFileSystem(job)
+    val fs = fileSplit.getPath.getFileSystem(context.getConfiguration)
     fs.copyToLocalFile(fileSplit.getPath, new Path(localFile.getAbsolutePath))
     // TODO(johnnyaug) should we cache this?
-    new EntryRecordReader(new SSTableReader(), localFile.getAbsolutePath)
+    new EntryRecordReader(new SSTableReader(localFile.getAbsolutePath, Catalog.Entry.getDefaultInstance))
   }
-
 }
