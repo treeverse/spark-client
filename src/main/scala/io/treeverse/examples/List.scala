df59ee093c394a2e881f666e7ae839ace85b8e61
johnnyaug <yoni.augarten@treeverse.io>
2021-02-22 16:02:03 +0200

    cr fix

--- src/main/scala/io/treeverse/examples/List.scala
+++ src/main/scala/io/treeverse/examples/List.scala
@@ -1,10 +1,7 @@
-package io.treeverse.clients.example
+package io.treeverse.examples
 
+import io.treeverse.clients.LakeFSContext
 import org.apache.spark.sql.SparkSession
-import io.treeverse.clients.{LakeFSContext, LakeFSInputFormat, WithIdentifier}
-import io.treeverse.catalog.Catalog
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
 
 object List extends App {
   private def dirs(path: String): Seq[String] =
@@ -13,13 +10,17 @@ object List extends App {
       .scanLeft("")((a, b: String) => a + "/" + b)
 
   override def main(args: Array[String]) {
+    if (args.length != 2) {
+      Console.err.println("Usage: ... <repo_name> <commit_id>")
+      System.exit(1)
+    }
     val spark = SparkSession.builder().master("local").appName("I can list")
       .config("spark.hadoop.lakefs.api.url", "http://localhost:8000/api/v1/")
       .config("spark.hadoop.lakefs.api.access_key", "AKIAIOSFODNN7EXAMPLE")
       .config("spark.hadoop.lakefs.api.secret_key", "walrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
       .getOrCreate()
     val sc = spark.sparkContext
-    val files = LakeFSContext.newRDD(sc, "example-repo", "c7be1745d24c72020001b4954fc33661652698b07a357832ff56137f68f63b0f")
+    val files = LakeFSContext.newRDD(sc, args(0), args(1))
     val count = files.flatMapValues(entry => dirs(entry.message.getAddress))
       .map({ case (_, s) => s })
       .countByValue
