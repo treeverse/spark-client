ab647aca3674ec7b1d1536ace8f1fd7997a2d929
Ariel Shaqed (Scolnicov) <ariels@treeverse.io>
2021-02-23 23:37:15 +0200

    Fix example: run where configured, reduce on workers
    Still does not write anything on a cluster (there seems nowhere that stdout _goes_), but runs
    to completion.

--- src/main/scala/io/treeverse/examples/List.scala
+++ src/main/scala/io/treeverse/examples/List.scala
@@ -14,18 +14,20 @@ object List extends App {
       Console.err.println("Usage: ... <repo_name> <commit_id>")
       System.exit(1)
     }
-    val spark = SparkSession.builder().master("local").appName("I can list")
+    val spark = SparkSession.builder().appName("I can list")
       .config("spark.hadoop.lakefs.api.url", "http://localhost:8000/api/v1/")
       .config("spark.hadoop.lakefs.api.access_key", "...")
       .config("spark.hadoop.lakefs.api.secret_key", "...")
       .getOrCreate()
     val sc = spark.sparkContext
-     val repo = args(0)
-     val ref = args(1)
-     val files = LakeFSContext.newRDD(sc, repo, ref)
+    val repo = args(0)
+    val ref = args(1)
+    val files = LakeFSContext.newRDD(sc, repo, ref)
+
     val count = files.flatMapValues(entry => dirs(entry.message.getAddress))
-      .map({ case (_, s) => s })
-      .countByValue
+      .map({ case (_, s) => (s, 1L) })
+      .reduceByKey(_ + _)
+
 
     count.collect({ case (path, count) => Console.printf("%s\t%d\n", path, count) })
 
